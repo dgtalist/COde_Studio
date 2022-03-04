1f70d86f04b7fc2c08ad47066e103f9436e5a71e
COde_Studio <31330881+dgtalist@users.noreply.github.com>
2022-03-04 14:57:57 +0900

    Create chapter02_01_menu.py

---
+++ RaspberryPi_python/chapter01_12 python/chapter02_01_menu.py
@@ -0,0 +1,20 @@
+'''chapter02_01_menu.py v1.0'''
+
+month = 6
+weeks = 4
+
+mon = '김치볶음밥'
+tues = '제육볶음'
+wednes = '된장찌게'
+thurs = '오므라이스'
+fri = '돈까스'
+
+print(str(month) + '월' + str(weeks) + '째주 식단입니다.')
+print()
+print('월요일 : ' + mon)
+print('화요일 : ' + tues)
+print('수요일 : ' + wednes)
+print('목요일 : ' + thurs)
+print('금요일 : ' + fri)
+print()
+print('맛있게 드세요.')
